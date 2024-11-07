66453bf7f120dc4eeb65b91a8fdee185250ed47a
Kaozhi Chen <kylerchen16@gmail.com>
2024-11-07 10:21:27 -0500

    display task list

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -47,6 +47,55 @@ class _HomePageState extends State<HomePage> {
           const SizedBox(
             height: 16,
           ),
+
+          // task list
+          Expanded(
+              child: StreamBuilder<QuerySnapshot>(
+                  stream: FirebaseFirestore.instance
+                      .collection('tasks')
+                      .snapshots(),
+                  builder: (context, snapshot) {
+                    if (!snapshot.hasData) {
+                      return const Center(
+                        child: CircularProgressIndicator(),
+                      );
+                    }
+
+                    // Filter tasks by selected date
+                    var tasks = snapshot.data!.docs.where((task) {
+                      DateTime taskDate = (task['date'] as Timestamp).toDate();
+                      return taskDate.year == _selectedDate.year &&
+                          taskDate.month == _selectedDate.month &&
+                          taskDate.day == _selectedDate.day;
+                    }).toList();
+
+                    if (tasks.isEmpty) {
+                      return const Center(
+                        child: Text('No tasks for today.'),
+                      );
+                    }
+                    return ListView.builder(
+                      itemCount: tasks.length,
+                      itemBuilder: (context, index) {
+                        var task = tasks[index];
+                        return Card(
+                          child: ListTile(
+                            title: Text(task['name']),
+                            subtitle: Text(
+                                "Priority: ${task['priority']}, Time: ${task['startTime']} - ${task['endTime']}"),
+                            trailing: Checkbox(
+                                value: task['status'],
+                                onChanged: (bool? value) {
+                                  FirebaseFirestore.instance
+                                      .collection('tasks')
+                                      .doc(task.id)
+                                      .update({'status': value});
+                                }),
+                          ),
+                        );
+                      },
+                    );
+                  }))
         ],
       ),
     );
