cc224a6c29bcafee17ab008e2c60b3cd7094d8fa
Kaozhi Chen <kylerchen16@gmail.com>
2024-11-07 13:52:33 -0500

    fix bug: add an empty space in the bottom of list view

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -62,124 +62,131 @@ class _HomePageState extends State<HomePage> {
       appBar: AppBar(
         title: const Text("Task Manager"),
       ),
-      body: Column(
-        children: [
-          // table calendar
-          TableCalendar(
-            firstDay: DateTime.utc(2020, 1, 1),
-            lastDay: DateTime.utc(2030, 12, 31),
-            focusedDay: _selectedDate,
-            selectedDayPredicate: (day) => isSameDay(day, _selectedDate),
-            calendarFormat: _calendarFormat,
-            onDaySelected: (selectedDay, focusedDay) {
-              setState(() {
-                _selectedDate = selectedDay;
-              });
-              // pass to MainScreen
-              widget.onDateSeleted(_selectedDate);
-            },
-            onFormatChanged: (format) {
-              setState(() {
-                _calendarFormat = format;
-              });
-            },
-            availableCalendarFormats: const {
-              CalendarFormat.month: 'Month',
-              CalendarFormat.week: 'Week'
-            },
-          ),
-          const SizedBox(
-            height: 16,
-          ),
+      body: SafeArea(
+        child: Column(
+          children: [
+            // table calendar
+            TableCalendar(
+              firstDay: DateTime.utc(2020, 1, 1),
+              lastDay: DateTime.utc(2030, 12, 31),
+              focusedDay: _selectedDate,
+              selectedDayPredicate: (day) => isSameDay(day, _selectedDate),
+              calendarFormat: _calendarFormat,
+              onDaySelected: (selectedDay, focusedDay) {
+                setState(() {
+                  _selectedDate = selectedDay;
+                });
+                // pass to MainScreen
+                widget.onDateSeleted(_selectedDate);
+              },
+              onFormatChanged: (format) {
+                setState(() {
+                  _calendarFormat = format;
+                });
+              },
+              availableCalendarFormats: const {
+                CalendarFormat.month: 'Month',
+                CalendarFormat.week: 'Week'
+              },
+            ),
+            const SizedBox(
+              height: 16,
+            ),
 
-          // task list
-          Expanded(
-              child: StreamBuilder<QuerySnapshot>(
-                  stream: FirebaseFirestore.instance
-                      .collection('tasks')
-                      .snapshots(),
-                  builder: (context, snapshot) {
-                    if (!snapshot.hasData) {
-                      return const Center(
-                        child: CircularProgressIndicator(),
-                      );
-                    }
-
-                    // Filter tasks by selected date
-                    var tasks = snapshot.data!.docs.where((task) {
-                      DateTime taskDate = (task['date'] as Timestamp).toDate();
-                      return taskDate.year == _selectedDate.year &&
-                          taskDate.month == _selectedDate.month &&
-                          taskDate.day == _selectedDate.day;
-                    }).toList();
-
-                    if (tasks.isEmpty) {
-                      return const Center(
-                        child: Text('No tasks for today.'),
-                      );
-                    }
-                    return ListView.builder(
-                      itemCount: tasks.length,
-                      itemBuilder: (context, index) {
-                        var task = tasks[index];
-                        return Column(
-                          children: [
-                            Card(
-                              child: ListTile(
-                                leading: Row(
-                                  mainAxisSize: MainAxisSize.min,
-                                  children: [
-                                    Container(
-                                      width: 6,
-                                      height: 50,
-                                      color: _getPriortyColor(task['priority']),
-                                    ),
-                                    const SizedBox(
-                                      width: 8,
-                                    ),
-                                    Checkbox(
-                                        value: task['status'],
-                                        onChanged: (bool? value) {
-                                          FirebaseFirestore.instance
-                                              .collection('tasks')
-                                              .doc(task.id)
-                                              .update({'status': value});
-                                        }),
-                                  ],
-                                ),
-                                title: Text(task['name']),
-                                subtitle: Text(
-                                    "${task['startTime']} - ${task['endTime']}"),
-                                trailing: Row(
-                                  mainAxisSize: MainAxisSize.min,
-                                  children: [
-                                    IconButton(
-                                      icon: const Icon(
-                                        Icons.edit,
-                                      ),
-                                      onPressed: () {
-                                        _showEditTaskBottomSheet(
-                                            taskId: task.id);
-                                      },
-                                    ),
-                                    IconButton(
-                                      icon: const Icon(
-                                        Icons.delete,
-                                      ),
-                                      onPressed: () {
-                                        _deleteTask(task.id);
-                                      },
-                                    ),
-                                  ],
-                                ),
-                              ),
-                            ),
-                          ],
+            // task list
+            Expanded(
+                child: StreamBuilder<QuerySnapshot>(
+                    stream: FirebaseFirestore.instance
+                        .collection('tasks')
+                        .snapshots(),
+                    builder: (context, snapshot) {
+                      if (!snapshot.hasData) {
+                        return const Center(
+                          child: CircularProgressIndicator(),
                         );
-                      },
-                    );
-                  }))
-        ],
+                      }
+
+                      // Filter tasks by selected date
+                      var tasks = snapshot.data!.docs.where((task) {
+                        DateTime taskDate =
+                            (task['date'] as Timestamp).toDate();
+                        return taskDate.year == _selectedDate.year &&
+                            taskDate.month == _selectedDate.month &&
+                            taskDate.day == _selectedDate.day;
+                      }).toList();
+
+                      if (tasks.isEmpty) {
+                        return const Center(
+                          child: Text('No tasks for today.'),
+                        );
+                      }
+                      return SafeArea(
+                        child: ListView.builder(
+                          padding: const EdgeInsets.only(bottom: 40),
+                          itemCount: tasks.length,
+                          itemBuilder: (context, index) {
+                            var task = tasks[index];
+                            return Column(
+                              children: [
+                                Card(
+                                  child: ListTile(
+                                    leading: Row(
+                                      mainAxisSize: MainAxisSize.min,
+                                      children: [
+                                        Container(
+                                          width: 6,
+                                          height: 50,
+                                          color: _getPriortyColor(
+                                              task['priority']),
+                                        ),
+                                        const SizedBox(
+                                          width: 8,
+                                        ),
+                                        Checkbox(
+                                            value: task['status'],
+                                            onChanged: (bool? value) {
+                                              FirebaseFirestore.instance
+                                                  .collection('tasks')
+                                                  .doc(task.id)
+                                                  .update({'status': value});
+                                            }),
+                                      ],
+                                    ),
+                                    title: Text(task['name']),
+                                    subtitle: Text(
+                                        "${task['startTime']} - ${task['endTime']}"),
+                                    trailing: Row(
+                                      mainAxisSize: MainAxisSize.min,
+                                      children: [
+                                        IconButton(
+                                          icon: const Icon(
+                                            Icons.edit,
+                                          ),
+                                          onPressed: () {
+                                            _showEditTaskBottomSheet(
+                                                taskId: task.id);
+                                          },
+                                        ),
+                                        IconButton(
+                                          icon: const Icon(
+                                            Icons.delete,
+                                          ),
+                                          onPressed: () {
+                                            _deleteTask(task.id);
+                                          },
+                                        ),
+                                      ],
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            );
+                          },
+                        ),
+                      );
+                    }))
+          ],
+        ),
       ),
     );
   }
